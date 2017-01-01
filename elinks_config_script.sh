d41959c7a1a6aa1d1bdaa02902ebfd127b08c1a8
krishnakumar Gopalakrishnan <krishnakumar@imperial.ac.uk>
2017-01-01 13:13:59 +0530

    Added more dotfiles.

---
+++ elinks_config_script.sh
@@ -0,0 +1,10 @@
+./configure --enable-bittorrent --enable-cgi \
+            --enable-finger --enable-fsp \
+            --enable-gopher --enable-nntp \
+            --enable-smb --enable-256-colors \
+            --enable-true-color --enable-exmode \
+            --enable-html-highlight --with-xterm \
+            --with-gc --with-lzma --with-gssapi \
+            --with-guile --with-perl --with-python \
+            --with-ruby --with-gnutls --with-openssl \
+            --with-x
