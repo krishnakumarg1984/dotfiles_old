6d860eb39ca5aa7b2ee87c137f12221af176e704
Krishna <krishnakumar@imperial.ac.uk>
2017-11-01 21:54:29 +0000

    update vim stuff

--- email_stuff/offlineimap.py
+++ email_stuff/offlineimap.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def mailpasswd():
-    path = "/home/gopalakrishnan/dotfiles/email_stuff/offlineimap_password.gpg"
+    path = "/home/kg314/dotfiles/email_stuff/offlineimap_password.gpg"
     return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
 
 remotepasseval = mailpasswd()
